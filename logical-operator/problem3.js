632dd85e35b52afe0bc4c1580f53651888506a9f
Shivam Mishra <smishra06542@gmail.com>
2022-10-15 18:49:22 +0530

    logical operator

---
+++ logical-operator/problem3.js
@@ -0,0 +1,9 @@
+//Given and character if it is a consonant print "Consonant"
+let char ="b";
+
+if (char == "b" || char == "c" || char == "d" || char == "f" || char == "g" || char == "h" || char == "j" || char =="k" || char =="l" || char =='m' || char =="n" || char == "p" || char == "q" || char == "r" || char =="t" || char =="v" || char =="w" || char =="x" || char =="y" || char == "z" ){
+  console.log("consonant");
+}
+else{
+  console.log("vowel")
+}
---
+++ logical-operator/problem4.js
@@ -0,0 +1,12 @@
+// Given 3 numbers (all different values), print which is greatest
+let a =10;
+let b =20;
+let c=30;
+
+if (a>b && a>c) {
+  console.log("a is greater");
+} else if (b>c && b>a) {
+  console.log("b is greater");
+} else {
+  console.log("c is greater");
+}
